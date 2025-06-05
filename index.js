1c1a909a1a69f7887c48f5b7a31a0185c475ffa5
Cristian Toj <tojcristian30@gmail.com>
2025-06-04 19:23:43 -0600

    Provocando error para probar CI/CD

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
+
 const express = require('express');
-const datos = require('./datos.json'); 
 const app = express();
-const PORT = 3000;
 
-app.get('/a', (req, res) => {
-  res.send('API CI/CD funcionando correctamente');
-});
-
-
-app.get('/estudiantes', (req, res) => {
-  res.json(datos);
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+app.get('/', (req, res) => {
+  res.send('API funcionando')
+})
